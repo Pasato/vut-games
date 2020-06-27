a6ed75ce2d259b8d9350bed0eec490a32551ce1b
pasatoai <aleksander@aims.ai>
2020-06-27 13:26:46 +0200

    initial board setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+const boardSize = 4;
+
+const boardStyle = {
+  display: 'grid',
+  margin: '0 auto',
+  position: 'relative',
+  top: '200px',
+  width: `${boardSize * 100 + (boardSize-1) * 10}px`,
+  gridTemplateColumns: `repeat(${boardSize}, 100px)`,
+  'column-gap': '10px',
+  'row-gap': '10px'
+};
+
+const cellStyle = {
+  display: 'flex',
+  'font-size': '3em',
+  'font-weight': 'bolder',
+  background: '#355C7D',
+  width: '100px',
+  height: '100px',
+  'justify-content': 'center',
+  'align-items': 'center',
+}
+
+const drawRandom = (noDrawn) => (available) => {
+  const indices = [];
+  while (indices.length < noDrawn) {
+    const randomInd = Math.floor(Math.random() * available.length);
+    if (!indices.includes(randomInd)) {
+      indices.push(randomInd);
+    }
+  }
+  return available.filter((_, i) => indices.includes(i));
+}
+
+//const draw1 = drawRandom(1);
+const draw2 = drawRandom(2);
+
+const toDrawRepresentation = (board) =>   
+  board.flat().map(x => x.pos);
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  created() {
+    this.board = Array.from(Array(boardSize), 
+      (_row, k) => Array.from(Array(boardSize),
+      (_col, j) => ({pos: [k, j], val: undefined})
+    ));
+    const [[firstRow, firstCol], [sndRow, sndCol]] = 
+      draw2(toDrawRepresentation(this.board));
+
+    this.board[firstRow][firstCol].val = 2;
+    this.board[sndRow][sndCol].val = 2;
+  },
+  mounted() {
+    document.body.addEventListener('keypress', (e) => console.log(e))
+  },
+  render() {
+    return (
+      <div style={boardStyle}>
+        {this.board.map((rows) => 
+          rows.map(({val}) => 
+          <div style={cellStyle}>
+            {val}
+          </div>
+          )
+        )}
+      </div>
+    )
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+  background: #F8B195;
+}
+#board {
+  display: 'grid';
+  gridTemplateColumns: 
+  
 }
 </style>
