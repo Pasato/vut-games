2d0cccc943f58708103cf94a779a7ef9a7721e4e
pasatoai <aleksander@aims.ai>
2020-06-29 21:22:45 +0200

    Up is working

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,16 @@ const cellStyle = {
   'align-items': 'center',
 }
 
+const matched = x => ({
+  on: () => matched(x),
+  otherwise: () => x,
+})
+
+const match = x => ({  
+  on: (pred, fn) => (pred(x) ? matched(fn(x)) : match(x)),
+  otherwise: fn => fn(x),
+})
+
 const drawRandom = (noDrawn) => (available) => {
   const indices = [];
   while (indices.length < noDrawn) {
@@ -38,34 +48,77 @@ const drawRandom = (noDrawn) => (available) => {
 //const draw1 = drawRandom(1);
 const draw2 = drawRandom(2);
 
-const toDrawRepresentation = (board) =>   
-  board.flat().map(x => x.pos);
+const getEmptyIndices = (board) => 
+  board.reduce((acc, row, rowInd) => 
+    [
+      ...acc, 
+      ...row.filter(col => !col).map((_col, colInd) => [rowInd, colInd])
+    ]
+  ,[]);
+
 
 export default {
   name: 'app',
+  data() {
+    return {
+      board: Array.from(Array(boardSize), 
+        () => Array.from(Array(boardSize),
+        () => undefined))
+    }
+  },
   created() {
-    this.board = Array.from(Array(boardSize), 
-      (_row, k) => Array.from(Array(boardSize),
-      (_col, j) => ({pos: [k, j], val: undefined})
-    ));
     const [[firstRow, firstCol], [sndRow, sndCol]] = 
-      draw2(toDrawRepresentation(this.board));
+      draw2(getEmptyIndices(this.board));
+    
+    this.setVal(firstRow, firstCol, 2);
+    this.setVal(sndRow, sndCol, 2);
+  },
+  methods: {
+    setVal: function(row, col, val) {
+      const newRow = this.board[row].slice(0);
+      newRow[col] = val;
+      this.$set(this.board, row, newRow);
+    }
 
-    this.board[firstRow][firstCol].val = 2;
-    this.board[sndRow][sndCol].val = 2;
   },
   mounted() {
-    document.body.addEventListener('keypress', (e) => console.log(e))
+    document.body.addEventListener('keyup', (e) => 
+      match(e)
+      .on(({key}) => key === 'ArrowUp', () => {
+        for (let k=0;k<boardSize;k++) {
+          for (let j=0;j<boardSize;j++) {
+            if (this.board[k][j]) {
+              for (let l=k-1;l>=0;l--) {
+                const [swapTo, swapFrom] = [l, l+1];
+                const [swapToVal, swapFromVal] = 
+                  [this.board[swapTo][j], this.board[swapFrom][j]];
+
+                if (!swapToVal && swapFromVal) {
+                  this.setVal(swapTo, j, swapFromVal);
+                  this.setVal(swapFrom, j, undefined)
+                } else if (swapToVal === swapFromVal) {
+                  this.setVal(swapTo, j, swapFromVal ** 2);
+                  this.setVal(swapFrom, j, undefined)
+                }
+              }
+
+            }
+          }
+        }
+        console.log(this.board);
+      })
+      .otherwise(() => {})
+    )
   },
   render() {
     return (
       <div style={boardStyle}>
-        {this.board.map((rows) => 
-          rows.map(({val}) => 
+        {this.board.map((row) => 
+          row.map(val => (
           <div style={cellStyle}>
             {val}
           </div>
-          )
+          ))
         )}
       </div>
     )
